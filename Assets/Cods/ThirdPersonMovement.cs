589ba805922eca693df2e68e88c2c2dd90a49693
Matheus <matheusfsantos.152@gmail.com>
2022-06-16 10:39:20 -0300

    Nova Programação personagem
    Novo programa para o personagem (falta ajuste)

--- Assets/Cods/ThirdPersonMovement.cs
+++ Assets/Cods/ThirdPersonMovement.cs
@@ -1,45 +1,98 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
 
-    public CharacterController controller;
-    public Transform cam;
-    public Rigidbody rb;
+    public CharacterController charctrl;
+    public Animator anim;
+    Vector3 movaxis, turnaxis;
+    public GameObject currentCamera;
+    public float jumpspeed = 8;
+    public float gravity = 20;
 
-    public float speed = 6f;
+    float yresult;
+   // public Transform rightHandObj, leftHandObj;
+    bool jumpbtn = false;
+    bool jumpbtnrelease = false;
 
-    public float turnSmoothTime = 0.1f;
-    float turnSmoothVelocity;
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked; // aperte esc para o cursor voltar
+        currentCamera = Camera.main.gameObject;     //busca a camera do jogador
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        if (Input.GetButtonDown("Jump"))     //se apertar pulo
+            jumpbtn = true; //habilita abooleana de pulo
 
-        if(Input.GetButtonDown("Jump"))
-        {
-            rb.AddForce(Vector3.up * 8f * 8f);
-        }
-       
-        if (direction.magnitude >= 0.1f)
-        {
 
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
-        }
+
+
+
     }
+
+    private void FixedUpdate()
+    {
+        movaxis = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); //captura o joystick e teclado
+
+        yresult -= gravity * Time.fixedDeltaTime;   //gravidade fica pesada
+
+        //calcula a direcao relativa do personagem em relacao ao mundo
+        Vector3 relativedirection = currentCamera.transform.TransformVector(movaxis);
+        relativedirection = new Vector3(relativedirection.x, yresult, relativedirection.z);
+        //remove o y da altura do vetor
+        Vector3 relativeDirectionWOy = relativedirection;
+        relativeDirectionWOy = new Vector3(relativedirection.x, 0, relativedirection.z);
+
+        anim.SetFloat("Speed", charctrl.velocity.magnitude);//seta a animacao de velocidade
+
+        //movimenta por character control
+        charctrl.Move(relativedirection * 0.1f);
+        //aplica a rotacao relativa
+        Quaternion rottogo = Quaternion.LookRotation(relativeDirectionWOy * 2 + transform.forward);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rottogo, Time.fixedDeltaTime * 50);
+
+        //se apertar o soco
+        if (Input.GetButtonDown("Fire1"))
+        {
+            anim.SetTrigger("PunchA"); //chama a animacao de soco
+        }
+        //se personagem está no chao
+        if (charctrl.isGrounded)
+        {
+            yresult = 0;    //zera a gravidade (evita bug de quada muito rapida) 
+        }
+        //se ele está no chao e apertou pulo
+        if (charctrl.isGrounded && jumpbtn)
+        {
+            anim.SetTrigger("Jump"); //chama animacao de pulo
+            yresult = jumpspeed;  //aplica a forca de pulo
+
+        }
+
+        //raycast pra detectar o chao e fazer o pulo proporcional
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position - (transform.forward * 0.1f) + transform.up * 0.3f, Vector3.down, out hit, 1000))
+        {
+            anim.SetFloat("JumpHeight", hit.distance);
+            if (hit.distance > 0.5f && jumpbtn)
+            {
+                
+                yresult = .1f;
+                
+                jumpbtn = false;
+                return;
+            }
+        }
+        jumpbtn = false;
+    }
+
+    
 }
