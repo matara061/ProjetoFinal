e9dced3d01951578c09c8c1a13bc2d719404c44f
lucasakiraa <101598190+lucasakiraa@users.noreply.github.com>
2022-06-28 11:00:33 -0300

    Ajustes Dungeon 2/Cod Destroy
    Complementando e montando a dungeon 2 com minions. player, câmera,alvos, e a sala de puzzle
    
    Adicionado o código para destroir um objeto

---
+++ Assets/Cods/Destroy.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+    // Start is called before the first frame update
+{
+    public int hp = 2;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Tiro")
+        {
+            hp--;
+            if (hp == 1)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
