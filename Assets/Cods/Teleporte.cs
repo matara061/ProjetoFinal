3c766f38a31743fa04c124fce124d5695362a82e
Petinho29 <petkovic.paiva@hotmail.com>
2022-07-04 16:34:16 -0300

    Correção Teleporte
    Implementação de parte do código realizado pelo @matara061, Adicionando apenas um couroutine e Inserindo um som ao executar a ação

--- Assets/Cods/Teleporte.cs
+++ Assets/Cods/Teleporte.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class Teleporte : MonoBehaviour
 {
     public GameObject ponto;
+
     [SerializeField]
     private GameObject player;
+
+    [SerializeField]
+    public ThirdPersonMovement personagem;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,9 +27,22 @@ public class Teleporte : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            StartCoroutine(CharacterControler());
+            FindObjectOfType<AudioManager>().Play("Teleporte");
             Debug.Log("teleport");
             player.transform.position = ponto.transform.position;
            
         }
+    }
+
+    //Vai desativar o character controler para o teleporte conseguir mudar a posição do player. Logo dps, vai ativar
+    IEnumerator CharacterControler()
+    {
+        personagem.charctrl.enabled = false;
+
+        yield return new WaitForSeconds(1f);
+
+        personagem.charctrl.enabled = true;
+
     }
 }
